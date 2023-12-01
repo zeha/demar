9b2260726cd744ebc288f6f844b1baa197bada73
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-12-01 17:20:19 +0100

    add tool to find source pkgs installing binary files

---
+++ find_sources_installing.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import pathlib
+import gzip
+from debian import deb822
+
+
+ARCHS = ["all", "arm64", "amd64"]
+
+
+def find_bin_pkgs_with_paths(contents: pathlib.Path) -> set[str]:
+    bin_pkgs = set()
+    with gzip.open(contents, "rt") as fp:
+        for line in fp:
+            path, packages = line.strip().split(maxsplit=1)
+            if path.startswith("lib/udev/rules"):
+                for package in packages.split(","):
+                    bin_pkgs.add(package.split("/")[1])
+
+    return bin_pkgs
+
+
+def main():
+    mirror = "/srv/debian-mirror/mirror"
+    bin_pkgs = set()
+    source_pkgs = set()
+
+    for arch in ARCHS:
+        bin_pkgs.update(find_bin_pkgs_with_paths(pathlib.Path(f"{mirror}/dists/sid/main/Contents-{arch}.gz")))
+
+    for arch in ARCHS:
+        pkglist_file = pathlib.Path(f"{mirror}/dists/sid/main/binary-{arch}/Packages.gz")
+        with gzip.open(pkglist_file, "rt") as pkglist:
+            for pkg in deb822.Packages.iter_paragraphs(pkglist):
+                bin_name = pkg["Package"]
+                if bin_name not in bin_pkgs:
+                    continue
+                source_pkgs.add(f"{pkg.source}_{pkg.source_version}")
+                bin_pkgs.remove(bin_name)
+
+    print("\n".join(sorted(source_pkgs)))
+
+    if bin_pkgs:
+        print("Unknown bins:", " ".join(sorted(bin_pkgs)))
+
+
+if __name__ == "__main__":
+    main()
