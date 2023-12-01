9b0a59f339787894e4f33eae81b23aacd05617a9
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-12-01 21:23:15 +0100

    Reformat

--- count_pkgs.py
+++ count_pkgs.py
@@ -3,17 +3,17 @@
 import re
 import sys
 
+
 def count_pkgs(s):
     seen = set()
     for line in s.splitlines():
-        m = re.match('/([^_]+).*.deb$', line.strip())
+        m = re.match("/([^_]+).*.deb$", line.strip())
         if m:
             pkg_name = m.group(1)
             seen.add(pkg_name)
     return len(seen)
-               
+
 
 for filename in sys.argv[1:]:
-    with open(filename, 'rt') as fp:
+    with open(filename, "rt") as fp:
         print(filename, count_pkgs(fp.read()))
-
