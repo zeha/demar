5cf257538fd70e0496d4aa07f00257fb00b44d10
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-12-01 18:20:22 +0100

    better finder

--- find_sources_installing.py
+++ find_sources_installing.py
@@ -5,6 +5,12 @@ from debian import deb822
 
 
 ARCHS = ["all", "arm64", "amd64"]
+COMPONENTS = ["main", "contrib", "non-free", "non-free-firmware"]
+
+IGNORE_SRC_PKGS = [
+    "linux",  # too big
+    "kino",  # FTBFS, rm pending
+]
 
 
 def find_bin_pkgs_with_paths(contents: pathlib.Path) -> set[str]:
@@ -24,18 +30,22 @@ def main():
     bin_pkgs = set()
     source_pkgs = set()
 
-    for arch in ARCHS:
-        bin_pkgs.update(find_bin_pkgs_with_paths(pathlib.Path(f"{mirror}/dists/sid/main/Contents-{arch}.gz")))
+    for component in COMPONENTS:
+        for arch in ARCHS:
+            bin_pkgs.update(
+                find_bin_pkgs_with_paths(pathlib.Path(f"{mirror}/dists/sid/{component}/Contents-{arch}.gz"))
+            )
 
-    for arch in ARCHS:
-        pkglist_file = pathlib.Path(f"{mirror}/dists/sid/main/binary-{arch}/Packages.gz")
-        with gzip.open(pkglist_file, "rt") as pkglist:
-            for pkg in deb822.Packages.iter_paragraphs(pkglist):
-                bin_name = pkg["Package"]
-                if bin_name not in bin_pkgs:
-                    continue
-                source_pkgs.add(f"{pkg.source}_{pkg.source_version}")
-                bin_pkgs.remove(bin_name)
+    for component in COMPONENTS:
+        for arch in ARCHS:
+            pkglist_file = pathlib.Path(f"{mirror}/dists/sid/{component}/binary-{arch}/Packages.gz")
+            with gzip.open(pkglist_file, "rt") as pkglist:
+                for pkg in deb822.Packages.iter_paragraphs(pkglist):
+                    bin_name = pkg["Package"]
+                    if bin_name not in bin_pkgs:
+                        continue
+                    source_pkgs.add(f"{pkg.source}_{pkg.source_version}")
+                    bin_pkgs.remove(bin_name)
 
     print("\n".join(sorted(source_pkgs)))
 
