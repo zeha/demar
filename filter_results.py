b6d185dfa4c395a7db9bc213f23348bb69648f21
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-12-17 14:27:43 +0100

    add tool to filter tally

---
+++ filter_results.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+from pathlib import Path
+import yaml
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="filter tallied results")
+    parser.add_argument("filename")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    with Path(args.filename).open("r") as fp:
+        data = list(yaml.safe_load_all(fp))
+
+    results = data[2]
+
+    filtered = {}
+
+    for src_name, detail in results.items():
+        if detail["groups"] == ["udev"] and detail["guessed_status"] not in (
+            "patch-in-bts",
+            "patch-marked-pending",
+            "bug-filed",
+        ):
+            filtered[src_name] = detail
+
+    yaml.safe_dump_all([filtered], sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
